4e2b7162a2086478807ac19c3d15c89864ce6897
hhariri <mail@hadihariri.com>
2012-11-09 14:42:46 +0100

    moved to structured constructs

--- src/Constructs.kt
+++ src/Constructs.kt
@@ -8,70 +8,28 @@ Retention(RetentionPolicy.RUNTIME) annotation class spec
 
 val eventAggregator = SimpleEventAggregator()
 
-class YOn<TGiven, TOn>
-{
-  fun yit(d: String, it: TOn.() -> Unit)
-    {
-
+public class Given {
+    public fun on(description: String, onExpression: On.() -> Unit) {
+        eventAggregator.publish(OnExecuted(description))
+        On().onExpression()
     }
 }
 
-class YGiven<TGiven>
-{
-    fun <TOn> yon(d: String, on: TGiven.() -> TOn) : YOn<TGiven, TOn>
-    {
-
-    }
-
-}
-
-fun <TGiven> ygiven(d : String, give: () -> TGiven) : YGiven<TGiven>
+public class On
 {
 
-}
-
-
-class TheOn
-{
-
-    fun xit(d: String, should: ()->Unit)
-    {
-
+    public fun it(description: String, itExpression: ()->Unit) {
+        try {
+            itExpression()
+            eventAggregator.publish(ItExecuted(description))
+        } catch (e: AssertionError) {
+            eventAggregator.publish(AssertionErrorOccurred(description, e))
+        }
     }
 }
 
-class TheGiven
-{
-    public fun xon(description: String, onExpression: TheOn.() -> Unit)
-    {
 
-    }
-}
-
-fun xgiven(description : String, givenExpression: TheGiven.() -> Unit)
-{
-    TheGiven().givenExpression()
-
-
-
-}
-
-
-fun on(description: String, onExpression: () -> Unit) {
-    eventAggregator.publish(OnExecuted(description))
-    onExpression()
-}
-
-fun it(description: String, itExpression: () -> Unit) {
-    try {
-        itExpression()
-        eventAggregator.publish(ItExecuted(description))
-    } catch (e: AssertionError) {
-        eventAggregator.publish(AssertionErrorOccurred(description, e))
-    }
-}
-
-fun given(description: String, givenExpression: () -> Unit) {
+public fun given(description : String, givenExpression: Given.() -> Unit) {
     eventAggregator.publish(GivenExecuted(description))
-    givenExpression()
+    Given().givenExpression()
 }
--- test/samples/calculator.kt
+++ test/samples/calculator.kt
@@ -2,67 +2,34 @@ package samples
 
 import spek.spec
 import spek.given
-import spek.on
-import spek.it
 import spek.shouldEqual
-import spek.ygiven
-import spek.xgiven
 
 
 spec public fun calculatorSpecs() {
 
 
-    given("a calculator", {
-        val calculator = Calculator()
-        on("calling sum with two numbers", {
-            val sum = calculator.sum(2, 4)
-            it("should return the result of adding the first number to the second", {
-                shouldEqual(7, sum)
-            })
-        })
-    })
-
-
-
-
-
-}
-
-fun xtext()
-{
-    xgiven("sdfsdf")
+    given("a calculator")
     {
-        val c = Calculator()
-        xon("sdjfbsdjf")
+        val calculator = Calculator()
+
+        on("calling sum with two numbers")
         {
-            val s = c.sum(7,5)
-            xit("should")
+
+            val sum = calculator.sum(2, 4)
+
+            it("should return the result of adding the first number to the second")
             {
-                shouldEqual(s, 12)
+                shouldEqual(7, sum)
             }
         }
-
-        xon("sdhfbsdhf")
-        {
-
-        }
     }
+
+
+
+
+
 }
 
-
-fun ytest()
-{
-    ygiven("Calculator")
-    {
-        Calculator()
-    }.yon("calling sum with two number")
-    {
-        sum(2,4)
-    }.yit("should")
-    {
-       shouldEqual(7)
-    }
-}
 
 class Calculator {
 
