a8a56ba16e5256c608b7f77ce310d4b059b38319
Mohammad Shamsi <m.h.shams@gmail.com>
2013-05-03 22:24:32 +0800

    introduced "AbstractSpek" to merge JUnit and Console Spek.

--- spec.junit/src/main/kotlin/API.kt
+++ spec.junit/src/main/kotlin/API.kt
@@ -4,11 +4,16 @@ import org.spek.impl.*
 import org.spek.junit.impl.*
 import org.junit.runner.RunWith
 import org.junit.Test as test
+import org.spek.console.api.ConsoleSpek
 
 RunWith(javaClass<JSpec<*>>())
-public abstract class JUnitSpek: SpekImpl() {
+public abstract class JUnitSpek: ConsoleSpek() {
 
     //possible workaround to cheat JUnit integration
     test public fun mockTest() {
     }
 }
+
+public abstract class AbstractSpek: JUnitSpek()
+
+
