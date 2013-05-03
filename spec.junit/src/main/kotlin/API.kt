2bf63880f3c78009dac9e5684af4ad394239e784
Mohammad Shamsi <m.h.shams@gmail.com>
2013-05-03 22:27:23 +0800

    removed extra modules & minor changes in samples

--- spec.junit/src/main/kotlin/API.kt
+++ spec.junit/src/main/kotlin/API.kt
@@ -7,13 +7,14 @@ import org.junit.Test as test
 import org.spek.console.api.ConsoleSpek
 
 RunWith(javaClass<JSpec<*>>())
-public abstract class JUnitSpek: ConsoleSpek() {
+public abstract class JUnitSpek: SpekImpl() {
 
     //possible workaround to cheat JUnit integration
     test public fun mockTest() {
     }
 }
 
-public abstract class AbstractSpek: JUnitSpek()
+RunWith(javaClass<JSpec<*>>())
+public abstract class AbstractSpek: SpekImpl()
 
 
