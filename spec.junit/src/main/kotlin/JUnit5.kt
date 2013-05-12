ab031daa8b7b3c9ab094622498674ad03134d835
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-05-12 14:55:03 +0200

    reuse console runner's code to search for test classes, implement another trick for JUnit tests to import all tests from package

---
+++ spec.junit/src/main/kotlin/JUnit5.kt
@@ -0,0 +1,21 @@
+package org.spek.junit.api
+
+import org.junit.runner.RunWith
+import org.junit.runner.Runner
+import org.junit.runner.Description
+import org.junit.runner.notification.RunNotifier
+import org.spek.console.reflect.SpecificationRunner
+import org.spek.junit.impl.JUnitLogger
+import org.junit.Test
+
+public open class JUnitMarker(_ : Class<*>) : Runner() {
+    private fun packageName() : String = getClass().getPackage()!!.getName()!!
+
+    public override fun getDescription(): Description? = Description.createSuiteDescription(packageName())
+
+    public override fun run(notifier: RunNotifier?) {
+        SpecificationRunner(JUnitLogger(notifier!!)).runSpecs(packageName())
+    }
+
+    Test public fun mockTest() {}
+}
