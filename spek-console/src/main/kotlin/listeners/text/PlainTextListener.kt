4133433b840133e3ed93c0126b7cc95cd4d885df
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-01-17 23:54:41 +0100

    include class name into console runner logs

--- spek-console/src/main/kotlin/listeners/text/PlainTextListener.kt
+++ spek-console/src/main/kotlin/listeners/text/PlainTextListener.kt
@@ -7,6 +7,13 @@ import org.spek.console.output.OutputDevice
 public class PlainTextListener(output: OutputDevice): Listener, OutputDevice by output {
     override fun spek(spek: String): StepListener {
         return object : StepListener {
+            override fun executionStarted() {
+                output(spek)
+            }
+            override fun executionFailed(error: Throwable) {
+                output("Failed: " + error.getMessage() + " " + error)
+                output("")
+            }
             override fun executionSkipped(why: String) {
                 output("Skipped Spek '$spek'. Reason: $why")
             }
@@ -19,19 +26,19 @@ public class PlainTextListener(output: OutputDevice): Listener, OutputDevice by 
     override fun given(given: String): StepListener {
         return object : StepListener {
             override fun executionStarted() {
-                output("Given $given")
+                output("  Given $given")
             }
             override fun executionCompleted() {
                 output("")
             }
             override fun executionSkipped(why: String) {
-                output("Skipped Given '$given'. Reason: $why")
+                output("  Skipped Given '$given'. Reason: $why")
             }
             override fun executionPending(why: String) {
-                output("Pending Given '$given'. Reason: $why")
+                output("  Pending Given '$given'. Reason: $why")
             }
             override fun executionFailed(error: Throwable) {
-                output("Failed: $error")
+                output("  Failed: $error")
                 output("")
             }
         }
@@ -40,17 +47,17 @@ public class PlainTextListener(output: OutputDevice): Listener, OutputDevice by 
     override fun on(given: String, on: String): StepListener {
         return object : StepListener {
             override fun executionStarted() {
-                output("  On $on")
+                output("    On $on")
             }
             override fun executionFailed(error: Throwable) {
-                output("  Failed: $error")
+                output("    Failed: $error")
                 output("")
             }
             override fun executionSkipped(why: String) {
-                output("  Skipped On '$on'. Reason: $why")
+                output("    Skipped On '$on'. Reason: $why")
             }
             override fun executionPending(why: String) {
-                output("  Pending On '$on'. Reason: $why")
+                output("    Pending On '$on'. Reason: $why")
             }
         }
     }
@@ -58,27 +65,19 @@ public class PlainTextListener(output: OutputDevice): Listener, OutputDevice by 
     override fun it(given: String, on: String, it: String): StepListener {
         return object : StepListener {
             override fun executionStarted() {
-                output("    It $it")
+                output("      It $it")
             }
             override fun executionFailed(error: Throwable) {
-                output("    Failed: $error")
+                output("      Failed: $error")
                 output("")
             }
             override fun executionSkipped(why: String) {
-                output("    Skipped It '$it'. Reason: $why")
+                output("      Skipped It '$it'. Reason: $why")
             }
             override fun executionPending(why: String) {
-                output("    Pending It '$it'. Reason: $why")
+                output("      Pending It '$it'. Reason: $why")
             }
         }
     }
 
-    ///TODO: implement stats
-    /*override fun notify(runStarted: RunStarted) {
-        main.output("Found ${runStarted.totalSpecifications} specification(s)\n\n")
-    }
-
-    override fun notify(runFinished: RunFinished) {
-        main.output("\n\nTotal passed: ${runFinished.passed} failed: ${runFinished.failed}")
-    }*/
 }
