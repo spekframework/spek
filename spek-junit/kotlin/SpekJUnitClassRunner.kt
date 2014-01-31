5352fc4277da2139c291a2d2dd08ac72cedcabd2
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-01-31 19:35:05 +0400

    Working towards working build

--- spek-junit/kotlin/SpekJUnitClassRunner.kt
+++ spek-junit/kotlin/SpekJUnitClassRunner.kt
@@ -5,6 +5,7 @@ import org.junit.runners.*
 import org.junit.runner.notification.*
 import org.spek.impl.*
 import org.spek.api.*
+import kotlin.properties.Delegates
 
 public fun testAction(description: Description, notifier: RunNotifier, action: () -> Unit) {
     notifier.fireTestStarted(description)
@@ -29,13 +30,15 @@ public class SpekJUnitOnRunner<T>(val specificationClass: Class<T>, val given: T
         return result
     }
 
-    override fun getDescription(): Description? {
+    val _description by Delegates.lazy {
         val desc = Description.createSuiteDescription(on.description())!!
         for (item in getChildren()) {
             desc.addChild(describeChild(item))
         }
-        return desc
+        desc
     }
+
+    override fun getDescription(): Description? = _description
 
     protected override fun describeChild(child: TestItAction?): Description? {
         val desc = Description.createSuiteDescription(child!!.description())
@@ -56,13 +59,15 @@ public class SpekJUnitGivenRunner<T>(val specificationClass: Class<T>, val given
         return result
     }
 
-    override fun getDescription(): Description? {
+    val _description by Delegates.lazy {
         val desc = Description.createSuiteDescription(given.description())!!
         for (item in getChildren()) {
             desc.addChild(describeChild(item))
         }
-        return desc
+        desc
     }
+
+    override fun getDescription(): Description? = _description
 
     protected override fun describeChild(child: SpekJUnitOnRunner<T>?): Description? {
         return child?.getDescription()
@@ -79,7 +84,6 @@ public class SpekJUnitClassRunner<T>(val specificationClass: Class<T>) : ParentR
     private val suiteDescription = Description.createSuiteDescription(specificationClass)
 
     override fun getChildren(): MutableList<SpekJUnitGivenRunner<T>> {
-
         if (javaClass<SpekImpl>().isAssignableFrom(specificationClass)) {
             val spek = specificationClass.newInstance() as SpekImpl
             val result = arrayListOf<SpekJUnitGivenRunner<T>>()
