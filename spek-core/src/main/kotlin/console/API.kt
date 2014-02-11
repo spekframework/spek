f4b12e357e9bd9ac0f90285f4d018d1cf02b1adf
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-02-11 17:30:24 +0400

    Simplify project structure

--- spek-core/src/main/kotlin/console/API.kt
+++ spek-core/src/main/kotlin/console/API.kt
@@ -1,6 +1,7 @@
 package org.spek.console.api
 
 import org.spek.impl.*
+import java.util.HashMap
 
 public abstract class ConsoleSpek: SpekImpl(), TestSpekAction {
     override fun description(): String = javaClass.getName()
