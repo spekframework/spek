aded620152b7cc10586877fd25c8409c747c88ce
Hadi Hariri <mail@hadihariri.com>
2014-08-11 08:18:10 +0200

    Refactoring beforeOn, afterOn tests and upgraded to latest snapshot of Kotlin.

--- spek-tests/src/test/kotlin/impl/OnBeforeAfterTest.kt
+++ spek-tests/src/test/kotlin/impl/OnBeforeAfterTest.kt
@@ -11,19 +11,51 @@ public class OnBeforeAfterTest : IntegrationTestCase() {
 
     test public fun callOnBefore() {
         val log = arrayListOf<String>()
-        runTest(object : Data() {{
-          given("a") {
-              beforeOn { log add "before"  }
-              afterOn { log add "after"  }
+        runTest(object: Data() {{
+            given("a") {
+                beforeOn { log add "before"}
+                on("1") {
+                    it("ddd") {}
+                    log add ("on")
+                }
 
-              on("1") {
-                  it("ddd") {}
-                  log add "on"
-              }
-              on("2") { it("zzz") { log add "it"; fail("rrr") } }
-          }
+            }
+        }})
+        assertEquals(arrayListOf("before", "on"), log)
+    }
+
+    test public fun callOnAfter() {
+        val log = arrayListOf<String>()
+        runTest(object: Data() {{
+            given("a") {
+                afterOn { log add "after"}
+                on("1") {
+                    it("ddd") {}
+                    log add ("on")
+                }
+
+            }
+        }})
+        assertEquals(arrayListOf("on", "after"), log)
+    }
+
+    test public fun callOnBeforeAndOnAfter() {
+        val log = arrayListOf<String>()
+        runTest(object : Data() {{
+            given("a") {
+                beforeOn { log add "before"  }
+                afterOn { log add "after"  }
+
+                on("1") {
+                    it("ddd") {}
+                    log add "on"
+                }
+                on("2") { it("zzz") { log add "it"; fail("rrr") } }
+            }
         }})
 
         Assert.assertEquals(arrayListOf("before", "on", "after", "before", "it", "after"), log)
     }
+
+
 }
