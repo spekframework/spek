d8d8f40193cbec8ed31850ad246afb0c3d50384e
jskierbi <jskierbi@gmail.com>
2016-01-29 19:34:39 +0100

    Added test data parametrization for testing both console runner and JUnit runner

--- spek-tests/src/test/kotlin/org/jetbrains/spek/api/OnBeforeAfterTest.kt
+++ spek-tests/src/test/kotlin/org/jetbrains/spek/api/OnBeforeAfterTest.kt
@@ -1,61 +1,102 @@
 package org.jetbrains.spek.api
-import org.junit.Test as test
-import kotlin.test.*
-import org.junit.*
-import org.jetbrains.spek.api.IntegrationTestCase.Data
 
-public class OnBeforeAfterTest : IntegrationTestCase() {
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.test.assertEquals
+import kotlin.test.fail
 
-    @test public fun callOnBefore() {
+@RunWith(JUnitParamsRunner::class)
+public class OnBeforeAfterTest {
+
+    @Test
+    @Parameters(source = SpekTestRunnerProvider::class)
+    public fun callOnBefore(runner: SpekTestRunner) {
         val log = arrayListOf<String>()
-        runTest(object: Data() {
+        runner.runTest(object : TestSpek() {
             init {
-            given("a") {
-                beforeOn { log.add("before")}
-                on("1") {
-                    it("ddd") {}
-                    log.add("on")
-                }
+                given("a") {
+                    beforeOn { log.add("before") }
+                    on("1") {
+                        it("ddd") {}
+                        log.add("on")
+                    }
 
+                }
             }
-        }})
+        })
         assertEquals(arrayListOf("before", "on"), log)
     }
 
-    @test public fun callOnAfter() {
+    @Test
+    @Parameters(source = SpekTestRunnerProvider::class)
+    public fun callOnAfter(runner: SpekTestRunner) {
         val log = arrayListOf<String>()
-        runTest(object: Data() {
+        runner.runTest(object : TestSpek() {
             init {
-            given("a") {
-                afterOn { log.add("after")}
-                on("1") {
-                    it("ddd") {}
-                    log.add("on")
-                }
+                given("a") {
+                    afterOn { log.add("after") }
+                    on("1") {
+                        it("ddd") {}
+                        log.add("on")
+                    }
 
+                }
             }
-        }})
+        })
         assertEquals(arrayListOf("on", "after"), log)
     }
 
-    @test public fun callOnBeforeAndOnAfter() {
+    @Test
+    @Parameters(source = SpekTestRunnerProvider::class)
+    public fun callOnBeforeAndOnAfter(runner: SpekTestRunner) {
         val log = arrayListOf<String>()
-        runTest(object : Data() {
+        runner.runTest(object : TestSpek() {
             init {
-            given("a") {
-                beforeOn { log.add("before")  }
-                afterOn { log.add("after")  }
+                given("1") {
+                    log.add("given1.start")
+                    beforeOn { log.add("before") }
+                    afterOn { log.add("after") }
 
-                on("1") {
-                    it("ddd") {}
-                    log.add("on")
+                    on("1") {
+                        log.add("on1.start")
+                        it("1") { log.add("on1.it1") }
+                        it("2") { log.add("on1.it2") }
+                        log.add("on1.end")
+                    }
+                    on("2") {
+                        log.add("on2.start")
+                        it("1") {
+                            log.add("on2.it1");
+                            fail("rrr")
+                        }
+                        it("2") { log.add("on2.it2") }
+                        log.add("on2.end")
+                    }
+                    log.add("given1.end")
                 }
-                on("2") { it("zzz") { log.add("it"); fail("rrr") } }
             }
-        }})
+        })
 
-        Assert.assertEquals(arrayListOf("before", "on", "after", "before", "it", "after"), log)
+        val expected = arrayListOf(
+                "given1.start",
+                "given1.end",
+                "before",
+                "on1.start",
+                "on1.end",
+                "on1.it1",
+                "on1.it2",
+                "after",
+                "before",
+                "on2.start",
+                "on2.end",
+                "on2.it1",
+                "on2.it2",
+                "after")
+
+        Assert.assertEquals(expected, log)
     }
-
 
 }
