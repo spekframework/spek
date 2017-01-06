f6f50adcfe091178d8995f048c6ea1173b71333a
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2017-01-06 09:00:57 +0800

    Don't allow fixtures within action scopes
    Fixes #154

--- spek-api/src/main/kotlin/org/jetbrains/spek/api/dsl/ActionBody.kt
+++ spek-api/src/main/kotlin/org/jetbrains/spek/api/dsl/ActionBody.kt
@@ -6,4 +6,25 @@ import org.jetbrains.spek.meta.SpekDsl
  * @author Ranie Jade Ramiso
  */
 @SpekDsl
-interface ActionBody: TestContainer
+interface ActionBody: TestContainer {
+
+    @Deprecated(message = "Can't be used within action scopes", level = DeprecationLevel.ERROR)
+    fun beforeEachTest(callback: () -> Unit) {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated(message = "Can't be used within action scopes", level = DeprecationLevel.ERROR)
+    fun afterEachTest(callback: () -> Unit) {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated(message = "Can't be used within action scopes", level = DeprecationLevel.ERROR)
+    fun beforeGroup(callback: () -> Unit) {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated(message = "Can't be used within action scopes", level = DeprecationLevel.ERROR)
+    fun afterGroup(callback: () -> Unit) {
+        throw UnsupportedOperationException()
+    }
+}
