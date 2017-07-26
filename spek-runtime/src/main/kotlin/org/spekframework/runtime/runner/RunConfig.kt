6eec4b8f06b22d1e4dab5a4fb4fa4cd50876ca78
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2017-07-26 21:42:19 +0800

    Remove unused code

--- spek-runtime/src/main/kotlin/org/spekframework/runtime/runner/RunConfig.kt
+++
@@ -1,7 +0,0 @@
-package org.spekframework.runtime.runner
-
-import org.spekframework.runtime.execution.RuntimeExecutionListener
-import org.spekframework.runtime.scope.Path
-
-data class RunConfig(val path: Path,
-                     val recorder: RuntimeExecutionListener)
