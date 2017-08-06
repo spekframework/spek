c75f6066081638ecea95e71ec3c0525205b8d4ce
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2017-08-06 21:22:10 +0800

    Remove old TestEngine

--- spek-junit-platform-engine/src/test/kotlin/org/jetbrains/spek/engine/packageWithAbstractSpek/SomeAbstractSpek.kt
+++
@@ -1,9 +0,0 @@
-/* Don't add anything in this package - [org.jetbrains.spek.engine.AbstractSpekTest] will fail. */
-package org.jetbrains.spek.engine.packageWithAbstractSpek
-
-import org.jetbrains.spek.api.Spek
-
-/**
- * @author Ranie Jade Ramiso
- */
-abstract class SomeAbstractSpek: Spek({})
