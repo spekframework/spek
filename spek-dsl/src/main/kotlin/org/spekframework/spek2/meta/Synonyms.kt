2c496242e301a9fc9c0fe3997c350b9c3a1a6ce9
Ranie Jade Ramiso <ranie@freelancer.com>
2017-08-18 15:52:48 +0800

    Add kdocs

--- spek-dsl/src/main/kotlin/org/spekframework/spek2/meta/Synonyms.kt
+++ spek-dsl/src/main/kotlin/org/spekframework/spek2/meta/Synonyms.kt
@@ -1,11 +1,22 @@
 package org.spekframework.spek2.meta
 
+/**
+ * Type of synonym.
+ */
 enum class SynonymType {
     Group,
     Action,
     Test
 }
 
+/**
+ * Marks a function as a synonym to either a group, action or test scope.
+ *
+ *
+ * @property type type of scope.
+ * @property prefix prefix appended to the description, if applicable.
+ * @property excluded whether the synonym represents an ignored scope.
+ */
 @Target(AnnotationTarget.FUNCTION)
 annotation class Synonym(val type: SynonymType,
                          val prefix: String = "",
