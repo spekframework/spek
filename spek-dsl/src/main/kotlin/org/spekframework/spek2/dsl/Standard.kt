cea78669e71b13cd4079f0afa2749e4c05ffe382
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2017-09-06 20:00:10 +0800

    Add meta annotations for IDE consumption (#249)
    Resolves #246

--- spek-dsl/src/main/kotlin/org/spekframework/spek2/dsl/Standard.kt
+++ spek-dsl/src/main/kotlin/org/spekframework/spek2/dsl/Standard.kt
@@ -1,11 +1,15 @@
 package org.spekframework.spek2.dsl
 
+import org.spekframework.spek2.meta.Synonym
+import org.spekframework.spek2.meta.SynonymType
+
 /**
  * Creates a [group][SpecBody.group].
  *
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Group, prefix = "describe ")
 fun SpecBody.describe(description: String, body: SpecBody.() -> Unit) {
     group("describe $description", body = body)
 }
@@ -16,6 +20,7 @@ fun SpecBody.describe(description: String, body: SpecBody.() -> Unit) {
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Group, prefix = "context ")
 fun SpecBody.context(description: String, body: SpecBody.() -> Unit) {
     group("context $description", body = body)
 }
@@ -26,6 +31,7 @@ fun SpecBody.context(description: String, body: SpecBody.() -> Unit) {
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Group, prefix = "given ")
 fun SpecBody.given(description: String, body: SpecBody.() -> Unit) {
     group("given $description", body = body)
 }
@@ -36,6 +42,7 @@ fun SpecBody.given(description: String, body: SpecBody.() -> Unit) {
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Action, prefix = "on ")
 fun SpecBody.on(description: String, body: ActionBody.() -> Unit) {
     action("on $description", body = body)
 }
@@ -46,6 +53,7 @@ fun SpecBody.on(description: String, body: ActionBody.() -> Unit) {
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Test, prefix = "it ")
 fun TestContainer.it(description: String, body: TestBody.() -> Unit) {
     test("it $description", body = body)
 }
@@ -56,6 +64,7 @@ fun TestContainer.it(description: String, body: TestBody.() -> Unit) {
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Group, prefix = "describe ", excluded = true)
 fun SpecBody.xdescribe(description: String, reason: String? = null, body: SpecBody.() -> Unit) {
     group("describe $description", Pending.Yes(reason), body = body)
 }
@@ -66,6 +75,7 @@ fun SpecBody.xdescribe(description: String, reason: String? = null, body: SpecBo
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Group, prefix = "context ", excluded = true)
 fun SpecBody.xcontext(description: String, reason: String? = null, body: SpecBody.() -> Unit) {
     group("context $description", Pending.Yes(reason), body = body)
 }
@@ -76,6 +86,7 @@ fun SpecBody.xcontext(description: String, reason: String? = null, body: SpecBod
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Group, prefix = "given ", excluded = true)
 fun SpecBody.xgiven(description: String, reason: String? = null, body: SpecBody.() -> Unit) {
     group("given $description", Pending.Yes(reason), body = body)
 }
@@ -86,6 +97,7 @@ fun SpecBody.xgiven(description: String, reason: String? = null, body: SpecBody.
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Action, prefix = "on ", excluded = true)
 fun SpecBody.xon(description: String, reason: String? = null, body: ActionBody.() -> Unit = {}) {
     action("on $description", Pending.Yes(reason), body = body)
 }
@@ -96,6 +108,7 @@ fun SpecBody.xon(description: String, reason: String? = null, body: ActionBody.(
  * @author Ranie Jade Ramiso
  * @since 1.0
  */
+@Synonym(type = SynonymType.Test, prefix = "it ", excluded = true)
 fun TestContainer.xit(description: String, reason: String? = null, body: TestBody.() -> Unit = {}) {
     test("it $description", Pending.Yes(reason), body)
 }
---
+++ spek-dsl/src/main/kotlin/org/spekframework/spek2/meta/Synonyms.kt
@@ -0,0 +1,23 @@
+package org.spekframework.spek2.meta
+
+/**
+ * Type of synonym.
+ */
+enum class SynonymType {
+    Group,
+    Action,
+    Test
+}
+
+/**
+ * Marks a function as a synonym to either a group, action or test scope.
+ *
+ *
+ * @property type type of scope.
+ * @property prefix prefix appended to the description (if applicable), this will appear in the test report.
+ * @property excluded whether the synonym represents an ignored scope.
+ */
+@Target(AnnotationTarget.FUNCTION)
+annotation class Synonym(val type: SynonymType,
+                         val prefix: String = "",
+                         val excluded: Boolean = false)
