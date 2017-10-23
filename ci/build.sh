555acc67b48a63301b74b00d58fb3a430448c618
Artem Zinnatullin :slowpoke <artem.zinnatullin@gmail.com>
2017-10-23 04:07:31 -0700

    Add --stacktrace.

--- ci/build.sh
+++ ci/build.sh
@@ -4,4 +4,4 @@ set -e
 # You can run it from any directory.
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
-"$PROJECT_DIR"/gradlew --no-daemon --info clean build
+"$PROJECT_DIR"/gradlew --no-daemon --info --stacktrace clean build
