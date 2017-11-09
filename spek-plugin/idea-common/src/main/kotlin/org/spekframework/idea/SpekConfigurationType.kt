abf3ae73b64a24b2e471e21cd34b5a3414523af3
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2017-11-09 22:55:02 +0800

    Setup initial structure

---
+++ spek-plugin/idea-common/src/main/kotlin/org/spekframework/idea/SpekConfigurationType.kt
@@ -0,0 +1,11 @@
+package org.spekframework.idea
+
+import com.intellij.execution.configurations.ConfigurationTypeBase
+import org.jetbrains.kotlin.idea.KotlinIcons
+
+abstract class SpekConfigurationType(id: String, displayName: String): ConfigurationTypeBase(
+    id,
+    displayName,
+    "Run specifications",
+    KotlinIcons.KOTLIN_LOGO_24
+)
