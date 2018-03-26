1db05cf74c683f8a3b9db42cf92052b4fe7222e6
Tarek Belkahia <tarek.belkahia@gmail.com>
2018-03-26 22:00:37 +0200

    Prevent nested tests and actions

--- spek-dsl/common/src/main/kotlin/org/spekframework/spek2/dsl/TestBody.kt
+++ spek-dsl/common/src/main/kotlin/org/spekframework/spek2/dsl/TestBody.kt
@@ -3,4 +3,19 @@ package org.spekframework.spek2.dsl
 import org.spekframework.spek2.meta.SpekDsl
 
 @SpekDsl
-interface TestBody
+interface TestBody {
+    @Deprecated(message = "Can't be used within test scopes", level = DeprecationLevel.ERROR)
+    fun test(description: String, pending: Pending = Pending.No, body: TestBody.() -> Unit) {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated(message = "Can't be used within test scopes", level = DeprecationLevel.ERROR)
+    fun it(description: String, body: TestBody.() -> Unit) {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated(message = "Can't be used within test scopes", level = DeprecationLevel.ERROR)
+    fun xit(description: String, reason: String? = null, body: TestBody.() -> Unit = {}) {
+        throw UnsupportedOperationException()
+    }
+}
