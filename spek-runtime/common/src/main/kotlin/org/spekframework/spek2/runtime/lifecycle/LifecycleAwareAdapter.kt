b3581cf3ea9b490699ccab2d7ce23cb39e037af0
Tarek Belkahia <tarek.belkahia@gmail.com>
2018-03-26 22:45:46 +0200

    Better generics handling

--- spek-runtime/common/src/main/kotlin/org/spekframework/spek2/runtime/lifecycle/LifecycleAwareAdapter.kt
+++ spek-runtime/common/src/main/kotlin/org/spekframework/spek2/runtime/lifecycle/LifecycleAwareAdapter.kt
@@ -10,28 +10,29 @@ import kotlin.reflect.KProperty
 sealed class LifecycleAwareAdapter<T>(val factory: () -> T, val destructor: (T) -> Unit)
     : LifecycleAware<T>, LifecycleListener {
 
-    protected sealed class Cached {
-        object Empty : Cached()
-        data class Value<out T>(val value: T) : Cached()
+    protected sealed class Cached<out T> {
+        object Empty : Cached<Nothing>()
+        data class Value<out T>(val value: T) : Cached<T>()
     }
 
-    protected fun <T> Cached.reified() = this as Cached.Value<T>
-
-    protected var cached: Cached = Cached.Empty
+    protected var cached: Cached<T> = Cached.Empty
 
     override fun getValue(thisRef: Any?, property: KProperty<*>) = invoke()
 
     override fun invoke(): T = when(cached) {
         Cached.Empty -> {
-            cached = Cached.Value(factory())
-            cached.reified<T>().value
+            val newCached = Cached.Value(factory())
+            cached = newCached
+            newCached.value
         }
-        is Cached.Value<*> -> cached.reified<T>().value
+        is Cached.Value<T> -> cached.cast().value
     }
+
+    protected fun Cached<T>.cast() = cached as Cached.Value<T>
 
     class GroupCachingModeAdapter<T>(factory: () -> T, destructor: (T) -> Unit)
         : LifecycleAwareAdapter<T>(factory, destructor) {
-        private val stack = mutableListOf<Cached>()
+        private val stack = mutableListOf<Cached<T>>()
 
         override fun beforeExecuteGroup(group: GroupScope) {
             stack.add(0, cached)
@@ -39,8 +40,8 @@ sealed class LifecycleAwareAdapter<T>(val factory: () -> T, val destructor: (T) 
         }
 
         override fun afterExecuteGroup(group: GroupScope) {
-            if (cached != Cached.Empty) {
-                destructor(cached.reified<T>().value)
+            if (cached is Cached.Value<T>) {
+                destructor(cached.cast().value)
             }
             if (stack.isNotEmpty()) {
                 cached = stack.removeAt(0)
@@ -52,8 +53,8 @@ sealed class LifecycleAwareAdapter<T>(val factory: () -> T, val destructor: (T) 
         : LifecycleAwareAdapter<T>(factory, destructor) {
         override fun afterExecuteGroup(group: GroupScope) {
             if (this.group == group) {
-                if (cached != Cached.Empty) {
-                    destructor(cached.reified<T>().value)
+                when (cached) {
+                    is Cached.Value<T> -> destructor(cached.cast().value)
                 }
                 cached = Cached.Empty
             }
@@ -64,16 +65,16 @@ sealed class LifecycleAwareAdapter<T>(val factory: () -> T, val destructor: (T) 
         : LifecycleAwareAdapter<T>(factory, destructor) {
         override fun afterExecuteTest(test: TestScope) {
             if (test.parent !is ActionScope) {
-                if (cached != Cached.Empty) {
-                    destructor(cached.reified<T>().value)
+                when (cached) {
+                    is Cached.Value<T> -> destructor(cached.cast().value)
                 }
                 cached = Cached.Empty
             }
         }
 
         override fun afterExecuteAction(action: ActionScope) {
-            if (cached != Cached.Empty) {
-                destructor(cached.reified<T>().value)
+            when (cached) {
+                is Cached.Value<T> -> destructor(cached.cast().value)
             }
             cached = Cached.Empty
         }
