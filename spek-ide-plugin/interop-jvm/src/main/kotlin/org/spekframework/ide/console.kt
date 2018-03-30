81722d03217a65768e706a6ce34e5186375a49ed
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-03-30 11:26:26 +1100

    ..

--- spek-ide-plugin/interop-jvm/src/main/kotlin/org/spekframework/ide/console.kt
+++ spek-ide-plugin/interop-jvm/src/main/kotlin/org/spekframework/ide/console.kt
@@ -1,16 +1,34 @@
 package org.spekframework.ide
 
 import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.mainBody
+import org.spekframework.spek2.runtime.SpekRuntime
+import org.spekframework.spek2.runtime.execution.DiscoveryRequest
+import org.spekframework.spek2.runtime.execution.ExecutionRequest
+import org.spekframework.spek2.runtime.scope.PathBuilder
 
 class Spek2ConsoleLauncher {
-    fun run(args: Array<String>) {
+    fun run(args: LauncherArgs) {
+        val paths = args.paths.map {
+            PathBuilder.parse(it)
+                .build()
+        }
+        val discoveryRequest = DiscoveryRequest(args.sourceDirs.toList(), paths)
+
+        val runtime =  SpekRuntime()
+
+        val discoveryResult = runtime.discover(discoveryRequest)
+        val executionRequest = ExecutionRequest(discoveryRequest)
+        runtime.e
     }
 }
 
-class LauncherArgs(prarser: ArgParser) {
-
+class LauncherArgs(parser: ArgParser) {
+    val sourceDirs by parser.adding("sourceDirs", help="Spec source dirs")
+    val paths by parser.adding("paths", help = "Spek paths to execute")
 }
 
-fun main(args: Array<String>) {
-
+fun main(args: Array<String>) = mainBody {
+    val launcherArgs = ArgParser(args).parseInto(::LauncherArgs)
+    Spek2ConsoleLauncher().run(launcherArgs)
 }
