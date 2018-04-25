cee6fd4ab1d5373dcbba9a10231e1f8c4bda2c2f
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-04-25 21:49:41 +1000

    checkpoint

--- spek-ide-plugin/intellij-common/src/main/kotlin/org/spekframework/intellij/SpekRunConfigurationProducer.kt
+++ spek-ide-plugin/intellij-common/src/main/kotlin/org/spekframework/intellij/SpekRunConfigurationProducer.kt
@@ -3,8 +3,11 @@ package org.spekframework.intellij
 import com.intellij.execution.actions.ConfigurationContext
 import com.intellij.execution.actions.RunConfigurationProducer
 import com.intellij.execution.configurations.ConfigurationTypeUtil
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.KotlinFacetSettingsProvider
+import org.jetbrains.kotlin.config.TargetPlatformKind
 
 class SpekRunConfigurationProducer: RunConfigurationProducer<SpekBaseRunConfiguration<*>>(
     ConfigurationTypeUtil.findConfigurationType(SpekBaseConfigurationType::class.java)
@@ -22,7 +25,18 @@ class SpekRunConfigurationProducer: RunConfigurationProducer<SpekBaseRunConfigur
         return if (path != null) {
             configuration.path = path
             configuration.setGeneratedName()
-            configuration.setModule(context.module)
+            val kotlinFacetSettings = checkNotNull(
+                KotlinFacetSettingsProvider.getInstance(context.project)
+                    .getSettings(context.module)
+            )
+
+            if (kotlinFacetSettings.targetPlatformKind == TargetPlatformKind.Common) {
+                val module = ModuleManager.getInstance(context.project)
+                    .findModuleByName(checkNotNull(kotlinFacetSettings.implementedModuleName))
+                configuration.setModule(module)
+            } else {
+                configuration.setModule(context.module)
+            }
             true
         } else {
             false
