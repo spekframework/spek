63984f4dc8a82313e72e38de9cdbaec5195b6082
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-04-29 14:36:48 +1000

    Checkpoint

--- spek-ide-plugin/intellij-common/src/main/kotlin/org/spekframework/intellij/SpekRunConfigurationProducer.kt
+++ spek-ide-plugin/intellij-common/src/main/kotlin/org/spekframework/intellij/SpekRunConfigurationProducer.kt
@@ -2,10 +2,12 @@ package org.spekframework.intellij
 
 import com.intellij.execution.actions.ConfigurationContext
 import com.intellij.execution.actions.RunConfigurationProducer
-import com.intellij.execution.configurations.ConfigurationTypeUtil
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.KotlinFacetSettings
 import org.jetbrains.kotlin.config.KotlinFacetSettingsProvider
 import org.jetbrains.kotlin.config.TargetPlatformKind
 
@@ -40,20 +42,35 @@ abstract class SpekRunConfigurationProducer(val producerType: ProducerType, conf
                 .getInitializedSettings(context.module)
 
             var canRun = false
-            if (kotlinFacetSettings.targetPlatformKind == TargetPlatformKind.Common) {
-                val module = ModuleManager.getInstance(context.project)
-                    .findModuleByName(checkNotNull(kotlinFacetSettings.implementedModuleNames.first()))
-                configuration.setModule(module)
-                canRun = true
-            } else if (isPlatformSupported(kotlinFacetSettings.targetPlatformKind!!)) {
+            if (isPlatformSupported(kotlinFacetSettings.targetPlatformKind!!)) {
                 configuration.setModule(context.module)
                 canRun = true
+            } else  if (kotlinFacetSettings.targetPlatformKind == TargetPlatformKind.Common) {
+                val (module, moduleKotlinFacetSettings) = findSupportedModule(context.project, kotlinFacetSettings)
+                configuration.setModule(module)
+                configuration.producerType = moduleKotlinFacetSettings.targetPlatformKind!!.toProducerType()
+                canRun = true
+            }
+
+            if (canRun) {
+                configuration.setGeneratedName()
             }
 
             canRun
         } else {
             false
         }
+    }
+
+    private fun findSupportedModule(project: Project, commonFacet: KotlinFacetSettings): Pair<Module, KotlinFacetSettings> {
+        val moduleManager = ModuleManager.getInstance(project)
+        val kotlinFacetSettingsProvider = KotlinFacetSettingsProvider.getInstance(project)
+
+        return commonFacet.implementedModuleNames.map { moduleManager.findModuleByName(it)!! }
+            .map { it to kotlinFacetSettingsProvider.getInitializedSettings(it) }
+            .first {
+                isPlatformSupported(it.second.targetPlatformKind!!)
+            }
     }
 
     private fun isPlatformSupported(targetPlatformKind: TargetPlatformKind<*>) = targetPlatformKind.toProducerType() == producerType
--- spek-ide-plugin/intellij-common/src/main/kotlin/org/spekframework/intellij/base.kt
+++ spek-ide-plugin/intellij-common/src/main/kotlin/org/spekframework/intellij/base.kt
@@ -8,7 +8,6 @@ import com.intellij.execution.configurations.ModuleBasedConfiguration
 import com.intellij.execution.configurations.RunConfigurationModule
 import com.intellij.openapi.util.JDOMExternalizerUtil
 import org.jdom.Element
-import org.jetbrains.kotlin.idea.KotlinIcons
 import org.spekframework.spek2.runtime.scope.Path
 import org.spekframework.spek2.runtime.scope.PathBuilder
 import org.spekframework.spek2.runtime.scope.isRoot
@@ -40,6 +39,8 @@ abstract class SpekBaseRunConfiguration<T: RunConfigurationModule>(name: String,
     private var programParameters: String? = null
 
     var path: Path = PathBuilder.ROOT
+
+    var producerType: ProducerType? = null
 
     override fun getWorkingDirectory() = workingDirectory
 
@@ -87,12 +88,16 @@ abstract class SpekBaseRunConfiguration<T: RunConfigurationModule>(name: String,
     override fun suggestedName(): String {
         val parent = path.parent
 
+        val prefix = producerType?.let {
+            "($it)"
+        } ?: ""
+
         return if (path.name.isEmpty()) {
-            "Specs in <default>"
+            "$prefix Specs in <default>"
         } else if (parent != null && parent.isRoot) {
-            "Specs in ${path.name}"
+            "S$prefix pecs in ${path.name}"
         } else {
-            path.name
-        }
+            "$prefix ${path.name}"
+        }.trim()
     }
 }
