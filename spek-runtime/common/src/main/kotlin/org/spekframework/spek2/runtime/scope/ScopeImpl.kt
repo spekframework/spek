fc16b3c983afad75737e279bcd65b52dbedd4928
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-05-21 23:06:57 +1000

    Allow tests to lookup memoized values

--- spek-runtime/common/src/main/kotlin/org/spekframework/spek2/runtime/scope/ScopeImpl.kt
+++ spek-runtime/common/src/main/kotlin/org/spekframework/spek2/runtime/scope/ScopeImpl.kt
@@ -2,12 +2,10 @@ package org.spekframework.spek2.runtime.scope
 
 import org.spekframework.spek2.dsl.Pending
 import org.spekframework.spek2.dsl.TestBody
-import org.spekframework.spek2.lifecycle.ActionScope
-import org.spekframework.spek2.lifecycle.GroupScope
-import org.spekframework.spek2.lifecycle.Scope
-import org.spekframework.spek2.lifecycle.TestScope
+import org.spekframework.spek2.lifecycle.*
 import org.spekframework.spek2.runtime.execution.ExecutionContext
 import org.spekframework.spek2.runtime.lifecycle.LifecycleManager
+import org.spekframework.spek2.runtime.lifecycle.MemoizedValueReader
 import kotlin.properties.ReadOnlyProperty
 
 sealed class ScopeImpl(val id: ScopeId,
@@ -101,7 +99,11 @@ class TestScopeImpl(id: ScopeId,
         lifecycleManager.beforeExecuteTest(this)
     }
     override fun execute(context: ExecutionContext) {
-        body.invoke(object: TestBody {})
+        body.invoke(object: TestBody {
+            override fun <T> memoized(): MemoizedValue<T> {
+                return MemoizedValueReader(this@TestScopeImpl)
+            }
+        })
     }
     override fun after(context: ExecutionContext) {
         lifecycleManager.afterExecuteTest(this)
