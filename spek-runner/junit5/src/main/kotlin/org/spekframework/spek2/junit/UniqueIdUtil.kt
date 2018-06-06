aba1389a7bdcf3d6c16e1fa4acd4884edc00c093
Artur Dryomov <ming13@users.noreply.github.com>
2018-06-07 01:56:43 +0300

    Move JUnit 5 engine ID to a single place. (#416)

--- spek-runner/junit5/src/main/kotlin/org/spekframework/spek2/junit/UniqueIdUtil.kt
+++ spek-runner/junit5/src/main/kotlin/org/spekframework/spek2/junit/UniqueIdUtil.kt
@@ -3,13 +3,12 @@ package org.spekframework.spek2.junit
 import org.junit.platform.engine.UniqueId
 import org.spekframework.spek2.runtime.scope.ScopeImpl
 
-val SPEK_ENGINE_UID = UniqueId.forEngine("spek")
+val SPEK_ENGINE_UID = UniqueId.forEngine(SpekTestEngine.ID)
 
 fun toUniqueId(scope: ScopeImpl?): UniqueId {
     return if (scope == null) {
         SPEK_ENGINE_UID
     } else {
         toUniqueId(scope.parent as ScopeImpl?).append("${scope.id.type}", scope.id.name)
-
     }
 }
