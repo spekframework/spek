a9d8d1b5d4104201729d76ee7ca11005e6fd70f4
Artur Dryomov <artur.dryomov@gmail.com>
2018-06-07 19:13:07 +0300

    Refactor JUnit runner.

--- spek-runner/junit5/src/main/kotlin/org/spekframework/spek2/junit/SpekPathDiscoverySelector.kt
+++ spek-runner/junit5/src/main/kotlin/org/spekframework/spek2/junit/SpekPathDiscoverySelector.kt
@@ -3,4 +3,4 @@ package org.spekframework.spek2.junit
 import org.junit.platform.engine.DiscoverySelector
 import org.spekframework.spek2.runtime.scope.Path
 
-class PathSelector(val path: Path): DiscoverySelector
+class SpekPathDiscoverySelector(val path: Path) : DiscoverySelector
