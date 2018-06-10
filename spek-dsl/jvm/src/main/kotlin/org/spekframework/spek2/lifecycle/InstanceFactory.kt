60c303fe065f2bf4bcce001c9d9b1e8b43c76226
Artur Dryomov <ming13@users.noreply.github.com>
2018-06-10 10:06:18 +0300

    Remove @since from documentation. (#419)

--- spek-dsl/jvm/src/main/kotlin/org/spekframework/spek2/lifecycle/InstanceFactory.kt
+++ spek-dsl/jvm/src/main/kotlin/org/spekframework/spek2/lifecycle/InstanceFactory.kt
@@ -4,9 +4,6 @@ import org.spekframework.spek2.Spek
 import org.spekframework.spek2.meta.Experimental
 import kotlin.reflect.KClass
 
-/**
- * @since 1.1
- */
 @Experimental
 interface InstanceFactory {
     fun <T: Spek> create(spek: KClass<T>): T
