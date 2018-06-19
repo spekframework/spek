348470c05f4b637435392c3a67d356ea74d5a254
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-06-19 19:39:46 +1000

    Rework gherkin style (#432)

--- integration-test/common/src/test/kotlin/org/spekframework/spek2/integration/SetFeature.kt
+++ integration-test/common/src/test/kotlin/org/spekframework/spek2/integration/SetFeature.kt
@@ -1,20 +1,20 @@
 package org.spekframework.spek2.integration
 
 import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
+import org.spekframework.spek2.style.gherkin.Given
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 object SetFeature: Spek({
-    Feature("Set") {
+    Given("Set") {
         val set by memoized { mutableSetOf<String>() }
 
-        Scenario("is empty") {
+        When("is empty") {
             Then("should have a size of 0") {
                 assertEquals(0, set.size)
             }
 
-            And("should throw when first is invoked") {
+            Then("should throw when first is invoked") {
                 assertFailsWith(NoSuchElementException::class) {
                     set.first()
                 }
