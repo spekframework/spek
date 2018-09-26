5e367396890480aa464e232c706c4208ef1d83a6
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-09-26 20:05:58 +1000

    Use virtualenv to build docs (#499)

---
+++ build_docs.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+set -e
+BASEDIR=$(dirname "$0")
+VIRTUALENV_DIR="$BASEDIR/p3"
+
+if [[ ! -d "$VIRTUALENV_DIR" ]]; then
+    pip install virtualenv
+    virtualenv -p python3 "$VIRTUALENV_DIR"
+    source "$VIRTUALENV_DIR/bin/activate"
+    pip install -r requirements.txt
+fi
+mkdocs build
