e70ab72df08acf077cc3233d99c30e976599c824
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-11-10 15:30:53 +1100

    Update samples to use latest version (#534)

--- samples/jvm/src/test/kotlin/org/spekframework/spek2/CalculatorTest.kt
+++ samples/jvm/src/test/kotlin/org/spekframework/spek2/CalculatorTest.kt
@@ -1,13 +1,11 @@
 package org.spekframework.spek2
 
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.Suite
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
 
 object CalculatorTest: Spek({
     describe("Calculator") {
-        val calculator by memoized<Calculator>()
+        val calculator by memoized { Calculator() }
 
         it("1 + 2 = 3") {
             assertEquals(3, calculator.add(1, 2))
