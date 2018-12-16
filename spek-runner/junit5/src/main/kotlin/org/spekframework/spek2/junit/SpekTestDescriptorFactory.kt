41f3aed52e822c3080140065b5be5bf7f8a27f99
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-12-17 10:56:03 +1100

    Avoid doing child recalculation for cached junit descriptor (#548)

--- spek-runner/junit5/src/main/kotlin/org/spekframework/spek2/junit/SpekTestDescriptorFactory.kt
+++ spek-runner/junit5/src/main/kotlin/org/spekframework/spek2/junit/SpekTestDescriptorFactory.kt
@@ -7,15 +7,25 @@ class SpekTestDescriptorFactory {
 
     private val cache = mutableMapOf<ScopeImpl, SpekTestDescriptor>()
 
-    fun create(scope: ScopeImpl): SpekTestDescriptor = createDescriptor(scope).apply {
-        if (scope is GroupScopeImpl) {
-            scope.getChildren().forEach { child ->
-                this.addChild(create(child))
+    fun create(scope: ScopeImpl): SpekTestDescriptor = createDescriptor(scope)
+
+    private fun createDescriptor(scope: ScopeImpl): SpekTestDescriptor {
+        var cached = true
+        val descriptor = cache.computeIfAbsent(scope) {
+            cached = false
+            SpekTestDescriptor(scope, this)
+        }
+
+        if (!cached) {
+            descriptor.apply {
+                if (scope is GroupScopeImpl) {
+                    scope.getChildren().forEach { child ->
+                        this.addChild(create(child))
+                    }
+                }
             }
         }
-    }
 
-    private fun createDescriptor(scope: ScopeImpl) = cache.computeIfAbsent(scope) {
-        SpekTestDescriptor(scope, this)
+        return descriptor
     }
 }
