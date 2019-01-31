d014858e4880afd27bae4be5c32887a163cb2b99
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2019-01-31 21:38:55 +1100

    Add support for AS 3.4

---
+++ spek-ide-plugin/android-studio/src/AS34/kotlin/org/spekframework/intellij/SpekAndroidRunConfiguration.kt
@@ -0,0 +1,48 @@
+package org.spekframework.intellij
+
+import com.android.tools.idea.run.PreferGradleMake
+import com.intellij.execution.Executor
+import com.intellij.execution.configurations.*
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.options.SettingsEditor
+import org.jdom.Element
+import org.spekframework.intellij.support.SpekJvmCommonRunConfigurationParameters
+import java.util.*
+
+class SpekAndroidRunConfiguration(name: String, module: JavaRunConfigurationModule, factory: ConfigurationFactory)
+    : ModuleBasedConfiguration<JavaRunConfigurationModule, RunConfigurationOptions>(name, module, factory)
+    , SpekBaseJvmRunConfiguration, PreferGradleMake {
+    override val data: SpekJvmCommonRunConfigurationParameters = SpekJvmCommonRunConfigurationParameters(project)
+
+    override fun getConfigurationEditor(): SettingsEditor<out RunConfiguration> {
+        return SpekAndroidSettingsEditor(project)
+    }
+
+    override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState? {
+        return SpekRunnnerCommandLineState(environment, this)
+    }
+
+    override fun getValidModules(): MutableCollection<Module> {
+        return mutableListOf(*ModuleManager.getInstance(project).modules)
+    }
+
+    override val configurationModule: JavaRunConfigurationModule
+        get() = super.getConfigurationModule()
+
+    override fun configureForModule(module: Module) {
+        setModule(module)
+        setGeneratedName()
+    }
+
+    override fun writeExternal(element: Element) {
+        super<ModuleBasedConfiguration>.writeExternal(element)
+        data.writeExternal(element)
+    }
+
+    override fun readExternal(element: Element) {
+        super<ModuleBasedConfiguration>.readExternal(element)
+        data.readExternal(element)
+    }
+}
