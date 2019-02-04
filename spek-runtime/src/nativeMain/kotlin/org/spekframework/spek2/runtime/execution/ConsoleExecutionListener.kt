a54277cb124977a4669c1110d63f6c810f777eeb
Charles Korn <me@charleskorn.com>
2019-02-04 11:14:13 +0900

    Display a short summary after the test run completes.

--- spek-runtime/src/nativeMain/kotlin/org/spekframework/spek2/runtime/execution/ConsoleExecutionListener.kt
+++ spek-runtime/src/nativeMain/kotlin/org/spekframework/spek2/runtime/execution/ConsoleExecutionListener.kt
@@ -4,12 +4,18 @@ import org.spekframework.spek2.runtime.scope.GroupScopeImpl
 import org.spekframework.spek2.runtime.scope.TestScopeImpl
 
 class ConsoleExecutionListener : ExecutionListener {
+    private var totalTests = 0
+    private var passedTests = 0
+    private var failedTests = 0
+    private var ignoredTests = 0
+    private var ignoredGroups = 0
+
     override fun executionStart() {
         println("START: execution")
     }
 
     override fun executionFinish() {
-        println("FINISH: execution")
+        println("FINISH: execution: $totalTests tests, $passedTests passed, $failedTests failed, $ignoredTests skipped. $ignoredGroups groups were skipped.")
     }
 
     override fun testExecutionStart(test: TestScopeImpl) {
@@ -18,11 +24,24 @@ class ConsoleExecutionListener : ExecutionListener {
 
     override fun testExecutionFinish(test: TestScopeImpl, result: ExecutionResult) =
             when (result) {
-                is ExecutionResult.Success -> println("PASSED: test: ${test.path}")
-                is ExecutionResult.Failure -> println("FAILED: test: ${test.path}: ${result.cause}")
+                is ExecutionResult.Success -> {
+                    totalTests++
+                    passedTests++
+
+                    println("PASSED: test: ${test.path}")
+                }
+                is ExecutionResult.Failure -> {
+                    totalTests++
+                    failedTests++
+
+                    println("FAILED: test: ${test.path}: ${result.cause}")
+                }
             }
 
     override fun testIgnored(test: TestScopeImpl, reason: String?) {
+        totalTests++
+        ignoredTests++
+
         println("IGNORED: test: ${test.path}: ${reason ?: "<no reason given>"}")
     }
 
@@ -37,6 +56,8 @@ class ConsoleExecutionListener : ExecutionListener {
             }
 
     override fun groupIgnored(group: GroupScopeImpl, reason: String?) {
+        ignoredGroups++
+
         println("IGNORED: group: ${group.path}: ${reason ?: "<no reason given>"}")
     }
 }
