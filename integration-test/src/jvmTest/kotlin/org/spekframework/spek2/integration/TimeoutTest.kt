72cd8740f4f850912a5302a2e0804cad17b2bebf
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2019-03-13 20:13:40 +1100

    remove ignore

--- integration-test/src/jvmTest/kotlin/org/spekframework/spek2/integration/TimeoutTest.kt
+++ integration-test/src/jvmTest/kotlin/org/spekframework/spek2/integration/TimeoutTest.kt
@@ -3,8 +3,6 @@ package org.spekframework.spek2.integration
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.meta.Ignore
 
-// remove @Ignore to test if timeouts are working.
-@Ignore
 object TimeoutTest: Spek({
     test("default timeout") {
         while (true) {}
