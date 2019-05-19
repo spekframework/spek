dba0d45540d6d3799597e66bfe40801af75da89b
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2019-05-19 10:12:58 +1000

    Add NATIVE and JS producer types in IJ

--- spek-ide-plugin/intellij-base/src/main/kotlin/org/spekframework/intellij/producerType.kt
+++ spek-ide-plugin/intellij-base/src/main/kotlin/org/spekframework/intellij/producerType.kt
@@ -2,17 +2,23 @@ package org.spekframework.intellij
 
 import org.jetbrains.kotlin.platform.IdePlatformKind
 import org.jetbrains.kotlin.platform.impl.CommonIdePlatformKind
+import org.jetbrains.kotlin.platform.impl.JsIdePlatformKind
 import org.jetbrains.kotlin.platform.impl.JvmIdePlatformKind
+import org.jetbrains.kotlin.platform.impl.NativeIdePlatformKind
 
 enum class ProducerType {
     COMMON,
-    JVM
+    JVM,
+    NATIVE,
+    JS
 }
 
 fun IdePlatformKind<*>.toProducerType(): ProducerType {
     return when (this) {
         CommonIdePlatformKind -> ProducerType.COMMON
         JvmIdePlatformKind -> ProducerType.JVM
+        NativeIdePlatformKind -> ProducerType.NATIVE
+        JsIdePlatformKind -> ProducerType.JS
         else -> throw IllegalArgumentException("Unsupported platform kind: ${this}")
     }
 }
