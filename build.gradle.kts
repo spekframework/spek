edee83c05a0f219cdcebdf3d25ea0ee22f65f7b4
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2019-06-22 19:12:41 +1000

    Progress

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
+plugins {
+    id("org.ajoberstar.reckon") version "0.9.0"
+}
+
+reckon {
+    scopeFromProp()
+    stageFromProp("alpha", "rc", "final")
+}
+
+
+allprojects {
+    repositories {
+        jcenter()
+    }
+
+    if (Files.exists(Paths.get("$rootDir/local.properties"))) {
+        val localProperties = java.util.Properties()
+        localProperties.load(java.io.FileInputStream("$rootDir/local.properties"))
+        localProperties.forEach { prop -> project.extra.set(prop.key as String, prop.value) }
+    }
+
+    var releaseMode = false
+    if ("$version".matches(Regex("^\\d+\\.\\d+\\.\\d+(-rc\\.\\d+)?"))) {
+        releaseMode = true
+    }
+    project.extra["releaseMode"] = releaseMode
+}
+
+listOf("check", "build", "clean").forEach { taskName ->
+   task(taskName) {
+       gradle.includedBuilds.forEach { includedBuild ->
+           dependsOn(gradle.includedBuild(includedBuild.name).task(":$taskName"))
+       }
+}
+}
---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
+}
+
+repositories {
+    jcenter()
+    gradlePluginPortal()
+}
+
+dependencies {
+    api(kotlin("gradle-plugin", version = "1.3.31"))
+    api("org.jetbrains.dokka:dokka-gradle-plugin:0.9.17")
+    api("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
+    api("gradle.plugin.org.jetbrains.intellij.plugins:gradle-intellij-plugin:0.4.8")
+    api("com.github.jengelman.gradle.plugins:shadow:4.0.2")
+}
