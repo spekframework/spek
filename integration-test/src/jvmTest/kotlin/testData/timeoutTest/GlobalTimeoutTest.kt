c9b0803fde7b85c5c96ad63b6a77cb5b0352153e
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2019-09-14 12:31:31 +1000

    .

--- integration-test/src/jvmTest/kotlin/testData/timeoutTest/GlobalTimeoutTest.kt
+++ integration-test/src/jvmTest/kotlin/testData/timeoutTest/GlobalTimeoutTest.kt
@@ -3,7 +3,7 @@ package testData.timeoutTest
 import org.spekframework.spek2.Spek
 
 object GlobalTimeoutTest : Spek({
-    test("this should run for 15 seconds and pass since global timeout is 20") {
-        sleep(15000)
+    test("this should run for 10 seconds and pass since global timeout is 20") {
+        sleep(10000)
     }
 })
