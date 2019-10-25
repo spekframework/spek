5e158de33a12edf90489a46de087987ce07f0372
marvinbuff <marvin.buff@optravis.com>
2019-10-25 17:02:26 +0200

    Fixed a wrong import

--- spek-runner-junit5/src/test/kotlin/org/spekframework/spek2/junit/SpekTestDescriptorFactoryTest.kt
+++ spek-runner-junit5/src/test/kotlin/org/spekframework/spek2/junit/SpekTestDescriptorFactoryTest.kt
@@ -6,11 +6,11 @@ import com.nhaarman.mockitokotlin2.mock
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.spekframework.spek2.dsl.Skip
+import org.spekframework.spek2.lifecycle.ScopeId
+import org.spekframework.spek2.lifecycle.ScopeType
 import org.spekframework.spek2.runtime.lifecycle.LifecycleManager
 import org.spekframework.spek2.runtime.scope.GroupScopeImpl
 import org.spekframework.spek2.runtime.scope.PathBuilder
-import org.spekframework.spek2.runtime.scope.ScopeId
-import org.spekframework.spek2.runtime.scope.ScopeType
 import kotlin.properties.Delegates
 
 class SpekTestDescriptorFactoryTest {
