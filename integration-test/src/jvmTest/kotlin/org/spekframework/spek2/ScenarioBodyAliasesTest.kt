72ef3701d4a934135336b02262a20d1fc4c84df1
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2019-11-09 00:02:50 +0100

    Introduce fixture aliases for Gherkin style (#818)

---
+++ integration-test/src/jvmTest/kotlin/org/spekframework/spek2/ScenarioBodyAliasesTest.kt
@@ -0,0 +1,35 @@
+package org.spekframework.spek2
+
+
+import org.spekframework.spek2.style.gherkin.ScenarioBody
+import kotlin.test.assertSame
+
+object ScenarioBodyAliasesTest : Spek({
+
+    val groupBody by memoized { FakeGroupBody() }
+    val scenarioBody by memoized { ScenarioBody(groupBody) }
+
+    test("beforeEachScenario should call BeforeEachGroup") {
+        val fixture = { println("hello") }
+        scenarioBody.beforeScenario(fixture)
+        assertSame(fixture, groupBody.beforeGroup)
+    }
+
+    test("afterEachScenario should call AfterEachGroup") {
+        val fixture = { println("hello") }
+        scenarioBody.afterScenario(fixture)
+        assertSame(fixture, groupBody.afterGroup)
+    }
+
+    test("beforeEachStep should call BeforeEachTest") {
+        val fixture = { println("hello") }
+        scenarioBody.beforeEachStep(fixture)
+        assertSame(fixture, groupBody.beforeEachTest)
+    }
+
+    test("afterEachStep should call AfterEachTest") {
+        val fixture = { println("hello") }
+        scenarioBody.afterEachStep(fixture)
+        assertSame(fixture, groupBody.afterEachTest)
+    }
+})
--- spek-dsl/src/commonMain/kotlin/org/spekframework/spek2/style/gherkin/gherkinStyle.kt
+++ spek-dsl/src/commonMain/kotlin/org/spekframework/spek2/style/gherkin/gherkinStyle.kt
@@ -1,10 +1,16 @@
 package org.spekframework.spek2.style.gherkin
 
+import org.spekframework.spek2.dsl.Fixture
 import org.spekframework.spek2.dsl.GroupBody
 import org.spekframework.spek2.dsl.LifecycleAware
 import org.spekframework.spek2.dsl.TestBody
 import org.spekframework.spek2.lifecycle.CachingMode
-import org.spekframework.spek2.meta.*
+import org.spekframework.spek2.meta.Description
+import org.spekframework.spek2.meta.DescriptionLocation
+import org.spekframework.spek2.meta.Descriptions
+import org.spekframework.spek2.meta.SpekDsl
+import org.spekframework.spek2.meta.Synonym
+import org.spekframework.spek2.meta.SynonymType
 
 @Synonym(SynonymType.GROUP, prefix = "Feature: ")
 @Descriptions(Description(DescriptionLocation.VALUE_PARAMETER, 0))
@@ -27,6 +33,23 @@ class FeatureBody(val delegate: GroupBody): LifecycleAware by delegate {
             body(ScenarioBody(this))
         }
     }
+
+    fun beforeEachScenario(fixture: Fixture) = delegate.beforeEachGroup(fixture)
+    fun afterEachScenario(fixture: Fixture) = delegate.afterEachGroup(fixture)
+    fun beforeFeature(fixture: Fixture) = delegate.beforeGroup(fixture)
+    fun afterFeature(fixture: Fixture) = delegate.afterGroup(fixture)
+
+    @Deprecated("Use beforeEachScenario instead", ReplaceWith("beforeEachScenario(fixture)"))
+    override fun beforeEachGroup(fixture: Fixture) = beforeEachScenario(fixture)
+
+    @Deprecated("Use afterEachScenario instead", ReplaceWith("afterEachScenario(fixture)"))
+    override fun afterEachGroup(fixture: Fixture) = afterEachScenario(fixture)
+
+    @Deprecated("Use beforeFeature instead", ReplaceWith("beforeFeature(fixture)"))
+    override fun beforeGroup(fixture: Fixture) = beforeFeature(fixture)
+
+    @Deprecated("Use afterFeature instead", ReplaceWith("afterFeature(fixture)"))
+    override fun afterGroup(fixture: Fixture) = afterFeature(fixture)
 }
 
 @SpekDsl
@@ -58,4 +81,21 @@ class ScenarioBody(val delegate: GroupBody): LifecycleAware by delegate {
     fun And(description: String, timeout: Long = defaultTimeout, body: TestBody.() -> Unit) {
         delegate.test("And: $description", timeout = timeout, body = body)
     }
+
+    fun beforeScenario(fixture: Fixture) = delegate.beforeGroup(fixture)
+    fun afterScenario(fixture: Fixture) = delegate.afterGroup(fixture)
+    fun beforeEachStep(fixture: Fixture) = delegate.beforeEachTest(fixture)
+    fun afterEachStep(fixture: Fixture) = delegate.afterEachTest(fixture)
+
+    @Deprecated("Use beforeEachStep instead", ReplaceWith("beforeEachStep(fixture)"))
+    override fun beforeEachTest(fixture: Fixture) = beforeEachStep(fixture)
+
+    @Deprecated("Use afterEachStep instead", ReplaceWith("afterEachStep(fixture)"))
+    override fun afterEachTest(fixture: Fixture) = afterEachStep(fixture)
+
+    @Deprecated("Use before scenario instead", ReplaceWith("beforeScenario(fixture)"))
+    override fun beforeGroup(fixture: Fixture) = beforeScenario(fixture)
+
+    @Deprecated("Use after scenario instead", ReplaceWith("afterScenario(fixture)"))
+    override fun afterGroup(fixture: Fixture) = afterScenario(fixture)
 }
