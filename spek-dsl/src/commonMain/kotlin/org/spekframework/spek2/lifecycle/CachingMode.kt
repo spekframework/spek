775ce888179221b83b1cdc2b42fb5a821415c90b
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-02-23 17:58:47 +1100

    Delete CachingMode.GROUP (#859)

--- spek-dsl/src/commonMain/kotlin/org/spekframework/spek2/lifecycle/CachingMode.kt
+++ spek-dsl/src/commonMain/kotlin/org/spekframework/spek2/lifecycle/CachingMode.kt
@@ -8,16 +8,6 @@ enum class CachingMode {
      * Each group will get their own unique instance. Nested groups will have
      * their own unique instance as well.
      */
-    @Deprecated(
-        "Use CachingMode.EACH_GROUP instead.",
-        replaceWith = ReplaceWith("org.spekframework.spek2.lifecycle.CachingMode.EACH_GROUP"),
-        level = DeprecationLevel.ERROR
-    )
-    GROUP,
-    /**
-     * Each group will get their own unique instance. Nested groups will have
-     * their own unique instance as well.
-     */
     EACH_GROUP,
 
     /**
