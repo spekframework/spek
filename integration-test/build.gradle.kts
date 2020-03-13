e0e06aa8db390663df34538e09fbeffca2972641
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-03-13 20:41:48 +1100

    .

--- integration-test/build.gradle.kts
+++ integration-test/build.gradle.kts
@@ -3,16 +3,6 @@ plugins {
   id("org.spekframework.spek2.multiplatform")
 }
 
-buildscript {
-  repositories {
-    jcenter()
-  }
-
-  dependencies {
-    classpath(kotlin("gradle-plugin", version = "1.3.61"))
-  }
-}
-
 repositories {
   jcenter()
 }
---
+++ integration-test/buildSrc/build.gradle.kts
@@ -0,0 +1,7 @@
+repositories {
+  jcenter()
+}
+
+dependencies {
+  api(kotlin("gradle-plugin", version = "1.3.61"))
+}
