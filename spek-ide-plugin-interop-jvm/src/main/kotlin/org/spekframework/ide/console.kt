24478254936d0de0710727ef0fbccfddd0e0c9c2
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-04-19 13:50:51 +1000

    Log exception when internal error occurs during test execution

--- spek-ide-plugin-interop-jvm/src/main/kotlin/org/spekframework/ide/console.kt
+++ spek-ide-plugin-interop-jvm/src/main/kotlin/org/spekframework/ide/console.kt
@@ -11,22 +11,27 @@ import kotlin.system.exitProcess
 
 class Spek2ConsoleLauncher {
     fun run(args: LauncherArgs) {
-        val paths = args.paths.map {
-            PathBuilder.parse(it)
-                .build()
+        try {
+            val paths = args.paths.map {
+                PathBuilder.parse(it)
+                    .build()
+            }
+            val context = JvmDiscoveryContextFactory.create(args.sourceDirs.toList())
+            val discoveryRequest = DiscoveryRequest(context, paths)
+
+            val runtime = SpekRuntime()
+
+            val discoveryResult = runtime.discover(discoveryRequest)
+            val executionRequest = ExecutionRequest(discoveryResult.roots, ServiceMessageAdapter())
+            runtime.execute(executionRequest)
+            // forces the jvm to shutdown even if there are non-daemon threads running.
+            // Ideally libraries should not leave non-daemon threads running after exit, unfortunately,
+            // this is quite common even with newer libraries.
+            exitProcess(0)
+        } catch (e: Throwable) {
+            println("An internal error has occurred.")
+            e.printStackTrace()
         }
-        val context = JvmDiscoveryContextFactory.create(args.sourceDirs.toList())
-        val discoveryRequest = DiscoveryRequest(context, paths)
-
-        val runtime = SpekRuntime()
-
-        val discoveryResult = runtime.discover(discoveryRequest)
-        val executionRequest = ExecutionRequest(discoveryResult.roots, ServiceMessageAdapter())
-        runtime.execute(executionRequest)
-        // forces the jvm to shutdown even if there are non-daemon threads running.
-        // Ideally libraries should not leave non-daemon threads running after exit, unfortunately,
-        // this is quite common even with newer libraries.
-        exitProcess(0)
     }
 }
 
