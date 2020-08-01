400afde5954a7568fb986451e6ac9c85a7898a19
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-08-01 19:30:52 +1000

    Remove verbose on cg scan and don't use MonoClock in jvm

--- spek-runtime/src/nativeMain/kotlin/org/spekframework/spek2/runtime/timeout.kt
+++ spek-runtime/src/nativeMain/kotlin/org/spekframework/spek2/runtime/timeout.kt
@@ -1,9 +1,18 @@
 package org.spekframework.spek2.runtime
 
+import kotlin.time.ExperimentalTime
+import kotlin.time.MonoClock
+import kotlin.time.measureTime
+
 actual fun getGlobalTimeoutSetting(default: Long): Long {
     return default
 }
 
 actual fun isConcurrentDiscoveryEnabled(default: Boolean): Boolean {
     return default
+}
+
+@UseExperimental(ExperimentalTime::class)
+actual fun measureTime(block: () -> Unit): Long {
+    return MonoClock.measureTime(block).inMilliseconds.toLong()
 }
