62d815598e3729e84931726fe1358ddf69b243d1
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-08-07 18:34:52 +1000

    Lock kotlin-reflection dep to 1.4.0-rc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,4 +17,5 @@ dependencies {
     api("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     api("gradle.plugin.org.jetbrains.intellij.plugins:gradle-intellij-plugin:0.4.10")
     api("com.github.jengelman.gradle.plugins:shadow:4.0.2")
+    implementation(kotlin("reflect", version = "1.4.0-rc"))
 }
