7f04164637399a0960fd47c6345276cde4ee8b35
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-08-27 23:07:17 +1000

    Upgrade to Kotlin 1.4 (#917)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,15 +7,13 @@ plugins {
 repositories {
     jcenter()
     gradlePluginPortal()
-    maven ("https://dl.bintray.com/kotlin/kotlin-eap")
-    maven ("https://kotlin.bintray.com/kotlinx")
 }
 
 dependencies {
-    api(kotlin("gradle-plugin", version = "1.4.0-rc"))
+    api(kotlin("gradle-plugin", version = "1.4.0"))
     api("org.jetbrains.dokka:dokka-gradle-plugin:1.4.0-rc")
     api("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
-    api("gradle.plugin.org.jetbrains.intellij.plugins:gradle-intellij-plugin:0.4.10")
+    api("gradle.plugin.org.jetbrains.intellij.plugins:gradle-intellij-plugin:0.4.21")
     api("com.github.jengelman.gradle.plugins:shadow:4.0.2")
-    implementation(kotlin("reflect", version = "1.4.0-rc"))
+    implementation(kotlin("reflect", version = "1.4.0"))
 }
--- integration-test/buildSrc/build.gradle.kts
+++ integration-test/buildSrc/build.gradle.kts
@@ -1,10 +1,8 @@
 repositories {
   jcenter()
-  maven ("https://dl.bintray.com/kotlin/kotlin-eap")
-  maven ("https://kotlin.bintray.com/kotlinx")
 }
 
 dependencies {
-  implementation(kotlin("gradle-plugin", version = "1.4.0-rc"))
-  implementation(kotlin("reflect", version = "1.4.0-rc"))
+  implementation(kotlin("gradle-plugin", version = "1.4.0"))
+  implementation(kotlin("reflect", version = "1.4.0"))
 }
